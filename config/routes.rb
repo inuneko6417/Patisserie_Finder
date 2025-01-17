5847c2ef10ba82a5d26d67ae387b4d6c69bda8e2
inuneko6417 <sao64179060@gmail.com>
2025-01-17 19:00:56 +0900

    トップ画面の作成しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   get "sessions/new"
-  get "pages/home"
   root "pages#home"
   resources :users, only: [ :new, :create ] # ユーザー登録に必要なルートのみ追加
+  resources :posts, only: [:index, :new, :create]
   get "signup", to: "users#new", as: :signup # 新規登録ページ用のカスタムルート
   get "login", to: "sessions#new"  # ログインページ（後で実装）
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
-
-
 
   get "up" => "rails/health#show", as: :rails_health_check
   # Render dynamic PWA files from app/views/pwa/*
