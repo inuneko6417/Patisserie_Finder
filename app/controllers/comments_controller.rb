cd29e6034676d17bd3b82ca5a8ef01944b904e74
inuneko6417 <sao64179060@gmail.com>
2025-03-03 10:10:00 +0900

    コメント機能を作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,15 +2,15 @@ class CommentsController < ApplicationController
   def create
     comment = current_user.comments.build(comment_params)
     if comment.save
-      redirect_to board_path(comment.board), success: t("コメント作成しました", item: Comment.model_name.human)
+      redirect_to post_path(comment.post), success: t("コメント作成しました", item: Comment.model_name.human)
     else
-      redirect_to board_path(comment.board), danger: t("コメント削除しました", item: Comment.model_name.human)
+      redirect_to post_path(comment.post), danger: t("コメント削除しました", item: Comment.model_name.human)
     end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:body).merge(board_id: params[:board_id])
+    params.require(:comment).permit(:body).merge(post_id: params[:post_id])
   end
 end
