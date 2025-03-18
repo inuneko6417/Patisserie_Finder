cc1ceca7468502c3e1202534be1490f901dc7465
inuneko6417 <sao64179060@gmail.com>
2025-03-18 10:23:08 +0900

    口コミランキングの基盤を作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,5 @@ class Post < ApplicationRecord
   validates :body, presence: true, length: { maximum: 1000 }
 
   scope :recent, -> { order(created_at: :desc) }
+  scope :by_comments_count, -> { left_joins(:comments).group(:id).order('COUNT(comments.id) DESC') }
 end
