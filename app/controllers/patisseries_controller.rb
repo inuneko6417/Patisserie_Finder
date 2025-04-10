190d64e5d3d49dd1f1ed533fce58667f94015c0c
inuneko6417 <sao64179060@gmail.com>
2025-04-10 16:17:06 +0900

    画像の追加

--- app/controllers/patisseries_controller.rb
+++ app/controllers/patisseries_controller.rb
@@ -2,15 +2,15 @@ class PatisseriesController < ApplicationController
   before_action :set_user
   def index
     @patisseries = [
-      { id: 1, name: "ル･ミリュウ 鎌倉山", description: "絶景カフェ併設" },
-      { id: 2, name: "パティスリー アンカド（patisserie un cadeau）", description: " 芸術的なケーキ" },
-      { id: 3, name: "パティスリーエスポワール", description: "地元に愛される店" },
-      { id: 4, name: "パティスリー・ラ・ベルデュール", description: "季節感あふれる洋菓子" },
-      { id: 5, name: "パティスリーテンアンド", description: "シンプル＆上品" },
-      { id: 6, name: "パティスリー サダハル アオキ パリ 丸の内店", description: "本場フランスの味" },
-      { id: 7, name: "パティスリーグランディール", description: "手作りの温かみ" },
-      { id: 8, name: "Patisserie Coeur&Heart", description: "ハート型スイーツ" },
-      { id: 9, name: "ラ·ブティック·ドゥ·ユキノシタ·カマクラ", description: "ミルフィーユが名物" }
+      { id: 1, name: "ル･ミリュウ 鎌倉山", description: "絶景カフェ併設", image: "le_milieu.png" },
+      { id: 2, name: "パティスリー アンカド（patisserie un cadeau）", description: " 芸術的なケーキ",image: "un_cadeau.png" },
+      { id: 3, name: "パティスリーエスポワール", description: "地元に愛される店", image: "espoir.png" },
+      { id: 4, name: "パティスリー・ラ・ベルデュール", description: "季節感あふれる洋菓子", image: "la_verdure.png" },
+      { id: 5, name: "パティスリーテンアンド", description: "シンプル＆上品", image: "ten&.png" },
+      { id: 6, name: "パティスリー サダハル アオキ パリ 丸の内店", description: "本場フランスの味", image: "sadaharu_aoki.png" },
+      { id: 7, name: "パティスリーグランディール", description: "手作りの温かみ", image: "grandir.png" },
+      { id: 8, name: "Patisserie Coeur&Heart", description: "ハート型スイーツ", image: "Coeur&Heart.png" },
+      { id: 9, name: "ラ·ブティック·ドゥ·ユキノシタ·カマクラ", description: "ミルフィーユが名物", image: "kamakura_yukinosita.png" }
     ]
   end
 
@@ -39,6 +39,7 @@ class PatisseriesController < ApplicationController
         closed_on: "年中無休",
         phone_number: "0467-50-0226",
         web_and_sns: "https://lemilieu-kamakurayama.com/free/kamakuraya",
+        image: "le_milieu.png",
         comments: [
           { user: "いぬねこ", text: "おいしかったです！", time: "2023/01/15 16:50" },
           { user: "ねこいぬ", text: "季節限定がおすすめです！", time: "2023/01/15 16:55" }
@@ -54,6 +55,7 @@ class PatisseriesController < ApplicationController
         closed_on: "火曜日",
         phone_number: "050-5462-0484",
         web_and_sns: "https://uncadeau.info",
+        image: "un_cadeau.png",
         comments: [
           { user: "いぬねこ", text: "おいしかったです！", time: "2023/01/15 16:50" },
           { user: "ねこいぬ", text: "季節限定がおすすめです！", time: "2023/01/15 16:55" }
@@ -69,6 +71,7 @@ class PatisseriesController < ApplicationController
         closed_on: "水曜日",
         phone_number: "046-229-2117",
         web_and_sns: "https://patisserie-espoir.shop",
+        image: "espoir.png",
         comments: [
           { user: "いぬねこ", text: "おいしかったです！", time: "2023/01/15 16:50" },
           { user: "ねこいぬ", text: "季節限定がおすすめです！", time: "2023/01/15 16:55" }
@@ -84,6 +87,7 @@ class PatisseriesController < ApplicationController
         closed_on: "月曜日",
         phone_number: "045-811-5123",
         web_and_sns: "https://www.verdure.co.jp/",
+        image: "la_verdure.png",
         comments: [
           { user: "いぬねこ", text: "おいしかったです！", time: "2023/01/15 16:50" },
           { user: "ねこいぬ", text: "季節限定がおすすめです！", time: "2023/01/15 16:55" }
@@ -99,6 +103,7 @@ class PatisseriesController < ApplicationController
         closed_on: "水曜日",
         phone_number: "03-6807-5622",
         web_and_sns: "https://www.parola.tokyo/",
+        image: "ten&.png",
         comments: [
           { user: "いぬねこ", text: "おいしかったです！", time: "2023/01/15 16:50" },
           { user: "ねこいぬ", text: "季節限定がおすすめです！", time: "2023/01/15 16:55" }
@@ -114,6 +119,7 @@ class PatisseriesController < ApplicationController
         closed_on: "水曜日",
         phone_number: "03-5293-2800",
         web_and_sns: "https://www.sadaharuaoki.jp/marunouchi/",
+        image: "sadaharu_aoki.png",
         comments: [
           { user: "いぬねこ", text: "おいしかったです！", time: "2023/01/15 16:50" },
           { user: "ねこいぬ", text: "季節限定がおすすめです！", time: "2023/01/15 16:55" }
@@ -129,6 +135,7 @@ class PatisseriesController < ApplicationController
         closed_on: "火,水曜日",
         phone_number: "046-264-7508",
         web_and_sns: "https://yyamato.com/patisserie-grandir/",
+        image: "grandir.png",
         comments: [
           { user: "いぬねこ", text: "おいしかったです！", time: "2023/01/15 16:50" },
           { user: "ねこいぬ", text: "季節限定がおすすめです！", time: "2023/01/15 16:55" }
@@ -144,6 +151,7 @@ class PatisseriesController < ApplicationController
         closed_on: "水曜日",
         phone_number: "042-860-2066",
         web_and_sns: "https://www.coeur-and-heart.info/",
+        image: "Coeur&Heart.png",
         comments: [
           { user: "いぬねこ", text: "おいしかったです！", time: "2023/01/15 16:50" },
           { user: "ねこいぬ", text: "季節限定がおすすめです！", time: "2023/01/15 16:55" }
@@ -159,6 +167,7 @@ class PatisseriesController < ApplicationController
         closed_on: "不定休",
         phone_number: "0467-539-692",
         web_and_sns: "https://www.sadaharuaoki.jp/marunouchi/",
+        image: "kamakura_yukinosita.png",
         comments: [
           { user: "いぬねこ", text: "おいしかったです！", time: "2023/01/15 16:50" },
           { user: "ねこいぬ", text: "季節限定がおすすめです！", time: "2023/01/15 16:55" }
