fc2eb715a17e2d1934fa311407c8fd89d74c2eb6
inuneko6417 <sao64179060@gmail.com>
2025-05-08 19:01:22 +0900

    .envファイルの修正しました

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,12 +5,12 @@ require "carrierwave/storage/fog"
 CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = "fog/aws"
-    config.fog_directory  = "arn:aws:s3:::patisserie-finder-production"
+    config.fog_directory  = ENV["AWS_BUCKET_NAME"]
     config.fog_credentials = {
       provider: "AWS",
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: "ap-northeast-1", # リージョン
+      region:  ENV["AWS_REGION"], # リージョン
       path_style: true
     }
 end
