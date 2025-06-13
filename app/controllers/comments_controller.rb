2c96d8210fa6dbb6e72bbb7174745bc04881ac52
inuneko6417 <sao64179060@gmail.com>
2025-06-13 19:43:26 +0900

    未ログイン時コメント打てないよう修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,25 @@
 class CommentsController < ApplicationController
   def create
-    @comment = current_user.comments.build(comment_params)
-    @comment.save
-  end
+      if user_signed_in?
+        @comment = current_user.comments.build(comment_params)
+        respond_to do |format|
+          if @comment.save
+            format.turbo_stream
+            format.html { redirect_to post_path(@comment.post), notice: t('defaults.comment_create_success') }
+          else
+            format.turbo_stream
+            format.html { redirect_to post_path(@comment.post), alert: t('defaults.comment_create_failed') }
+          end
+        end
+      else
+        @comment = Comment.new # 空のコメントをセット
+        flash.now[:alert] = t('defaults.login_required')
+        respond_to do |format|
+          format.turbo_stream { render turbo_stream: turbo_stream.replace("comment-form", partial: "comments/form", locals: { comment: @comment, post: Post.find(params[:post_id]) }) }
+          format.html { redirect_to login_path, alert: t('defaults.login_required') }
+        end
+      end
+    end
 
   def destroy
     @comment = current_user.comments.find(params[:id])
